9b81e41011da016e24b9edc48972e37cec7c8a1c
shubham.jangilwad@eppsinfotech.in <shubhamjangilwad9763@gmail.com>
2026-08-18 12:27:56 +0530

    created payment module and migrate foreign key tenants.id

--- app/models/payments.py
+++ app/models/payments.py
@@ -6,12 +6,12 @@ class Payment(Base):
     __tablename__ = "payments"
 
     id = Column(Integer , primary_key= True , index=True)
-    tenant_id = Column(Integer , ForeignKey("rooms.id"), nullable=False)
+    tenant_id = Column(Integer , ForeignKey("tenants.id"), nullable=False)
     payment_month = Column(String(10) , nullable=False)
     amount = Column(Integer , nullable=False)
     payment_date = Column(Date , nullable=False)
     payment_status = Column(String(20) , nullable=False)
-    refund_amount = Column(Integer , nullable=False)
-    maintenance_charge = Column(Integer, nullable=False)
+    refund_amount = Column(Integer , nullable=True)
+    maintenance_charge = Column(Integer, nullable=True)
     is_active = Column(Boolean , default=True)
     created_at = Column(DateTime , server_default=func.now())
