60f40f7e8be2f0b59ee76692298ce2db73029bb0
shubham.jangilwad@eppsinfotech.in <shubhamjangilwad9763@gmail.com>
2026-08-21 19:14:06 +0530

    upd room pric

--- app/routers/room_pricing_router.py
+++ app/routers/room_pricing_router.py
@@ -1,18 +1,25 @@
 from fastapi import APIRouter , Depends 
 from sqlalchemy.orm import Session
-from app.schemas.room_pricing_schema import RoomPricingCreate , RoomPricingResponse
+from app.schemas.room_pricing_schema import RoomPricingCreate , RoomPricingResponse, RoomPricingUpdateSchema
 from app.database import get_db
 from app.services.auth import get_current_user
-from app.services.room_pricing_service import create_room_pricing_service
+from app.services.room_pricing_service import create_room_pricing_service , update_room_pricing_service
 
 
 
 
 RoomPricing = APIRouter() 
 
-@RoomPricing.post("/create/roompri", response_model=RoomPricingResponse)
+@RoomPricing.post("/create/roompric", response_model=RoomPricingResponse)
 def create_room_pricing(body : RoomPricingCreate,
                         current_user = Depends(get_current_user),
                         db : Session = Depends(get_db)):
 
     return create_room_pricing_service(body,current_user,db)
+
+@RoomPricing.put("/update/roompric",response_model=RoomPricingResponse)
+def update_room_pricing(
+                        body : RoomPricingUpdateSchema,
+                        current_user = Depends(get_current_user),
+                        db : Session = Depends(get_db)):
+    return update_room_pricing_service(body,current_user,db)
